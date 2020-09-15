b5a464f790f7cdc1010075be71f94e4918620151
Hari Shyam <hari.shyam.942@gmail.com>
2020-09-15 23:21:05 +0530

    v0.1

---
+++ precache-manifest.24fe18a545d6b07fcc37339f5ba9088b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e734e5a4ab2507182fc3ff4d4c1df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdef5d1722c5aea60517",
+    "url": "/static/css/main.cddc3cf3.chunk.css"
+  },
+  {
+    "revision": "8e29b13e6c47d200bf1f",
+    "url": "/static/js/2.639491ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.639491ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdef5d1722c5aea60517",
+    "url": "/static/js/main.0d9d46d5.chunk.js"
+  },
+  {
+    "revision": "23754a29c5e798b2c0a5",
+    "url": "/static/js/runtime-main.deb80fbb.js"
+  },
+  {
+    "revision": "90f0a6dd7d29a276b75b97478473640a",
+    "url": "/static/media/PhageRough.90f0a6dd.woff2"
+  },
+  {
+    "revision": "d213f3d98b8a395ad8c75537515e0f0b",
+    "url": "/static/media/bg-square-tiled-2.d213f3d9.png"
+  },
+  {
+    "revision": "4513c5e24dd0e2c42f77801dde975394",
+    "url": "/static/media/logo.4513c5e2.svg"
+  }
+]);
